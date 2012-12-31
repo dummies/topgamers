913d2cc9d9c86c439c835463f095ade08aa1ea4c
dummies <saisreenivasan@live.in>
2012-12-31 14:04:44 +0530

    fb pic

--- Lboard.php
+++ Lboard.php
@@ -1,4 +1,5 @@
-<html lang="en-us" class=" js no-flexbox flexbox-legacy no-touch rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent cssmask" dir="ltr"><head>
+<html lang="en-us" class=" js no-flexbox flexbox-legacy no-touch rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent cssmask" dir="ltr">
+<head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <title>All time top gamers</title>
 <link href="/main.css" rel="stylesheet">
@@ -73,7 +74,6 @@
 
 <div id="container">
 	<h1> Top Players</h1>
-		
 	<div class="panel"><div class="hover"></div></div>
 	<div class="panel"><div class="hover"></div></div>
 	<div class="panel"><div class="hover"></div></div>
@@ -100,6 +100,53 @@
 	} catch(err) {}
 </script>-->
 
+<?php
+
+	try {
+    $conn = new PDO ( "sqlsrv:server = tcp:pvp6ee8yc7.database.windows.net,1433; Database = gamer_scores", "dummies", "dumm!es3");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+   catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
+    $sql_select = "SELECT * FROM leaderboard order by Score desc";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+	var_dump($registrants);
+	//echo "<br/> I am fetching <br/> ";
+    if(count($registrants) > 0)
+	{
+		//echo count($registrants);
+        echo "<div id='container'><h1> Top Players</h1>";
+		$i=0;
+		$prev=NULL;
+        foreach($registrants as $registrant) 
+		{
+			//var_dump($registrant);
+			if($i < 8)	
+			{
+			echo "<div class='panel'>";
+            /*echo "<tr><th>".$i."</th>";
+			echo "<th>".$registrant['Name']."</th>";
+            echo "<th><th>".$registrant['Score']."</th></th></tr>";*/
+			$tmp = "https://graph.facebook.com/".$registrant['id']."/picture";
+			echo "<img src='$tmp'>";
+			echo "<div class='hover'></div></div>";
+			}
+			$i++;
+        }
+        echo "</table>";
+    } 
+	else
+	{
+        echo "<h3>Oops,Looks like its sleep time ,nobody is playing! </h3>";
+     }
+	
+
+?>
 
 
-</body></html>
+
+</body>
+</html>
